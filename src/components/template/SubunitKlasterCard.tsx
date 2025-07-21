1e4b860fe803c4eafa9fe3863e29007453d86238
prasetyoedi <prasetyoedi451@gmail.com>
2025-07-21 16:53:12 +0700

    refactor: remove unnecessary comments in SubunitKlasterCard component

--- src/components/template/SubunitKlasterCard.tsx
+++ src/components/template/SubunitKlasterCard.tsx
@@ -1,7 +1,5 @@
 import { H2 } from "@/components";
 
-// ini juga bisa dipakai untuk cluster
-
 export function SubunitKlasterCard(props: any) {
   const { image, title, description } = props;
   return (
@@ -10,15 +8,10 @@ export function SubunitKlasterCard(props: any) {
         className="relative overflow-hidden rounded-[24px] bg-cover bg-center text-white shadow-md transition-transform duration-300 hover:scale-[1.03]"
         style={{ backgroundImage: `url(${image})` }}
       >
-        {/* Overlay gelap untuk meningkatkan keterbacaan teks */}
         <div className="bg-black/40 absolute inset-0"></div>
-
-        {/* Judul Tengah di Atas */}
         <H2 className="relative z-10 pb-10 pt-[200px] text-center font-alatsi text-xl sm:text-2xl md:text-3xl lg:text-4xl">
           {title}
         </H2>
-
-        {/* Konten Deskripsi */}
         <div className="relative z-10 px-6 pb-6 font-jakarta text-sm sm:text-base">
           <p className="font-semibold">{title}</p>
           <p className="text-white/90">{description}</p>
