0c103a0b59e5b27f89199abfe21d0a6d67b53859
prasetyoedi <prasetyoedi451@gmail.com>
2025-07-21 16:40:54 +0700

    feat: improve text styling and layout in Klaster page

--- src/components/pages/Klaster.tsx
+++ src/components/pages/Klaster.tsx
@@ -44,28 +44,26 @@ export function Klaster() {
       />
       <div data-aos="fade-up">
         <Container className="pt-[101px] font-jakarta">
-          <H1 className="text-center font-bold">Let’s See Our Klaster!</H1>
-          <H3 className="pt-[24px] text-center">
+          <H1 className="text-center text-2xl font-bold md:text-3xl lg:text-4xl">
+            Let’s See Our Klaster!
+          </H1>
+          <H3 className="pt-6 text-center text-base md:text-lg lg:text-xl">
             Berikut adalah Klaster dari Melukis Pakis di Desa Pogalan dan
             Ketundan
           </H3>
           <div
-            className="mb-[100px] mt-[48px] grid grid-cols-1 gap-x-[28px] gap-y-[40px] sm:grid-cols-2 xl:grid-cols-4"
+            className="mb-[100px] mt-12 grid grid-cols-1 gap-x-7 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
             data-aos="fade-up"
           >
-            {dataKlaster.map(({ image, title, href, description }: any) => {
-              return (
-                <>
-                  <a href={`/klaster/${href}`} key={title}>
-                    <SubunitKlasterCard
-                      image={image}
-                      title={title}
-                      description={description}
-                    />
-                  </a>
-                </>
-              );
-            })}
+            {dataKlaster.map(({ image, title, href, description }: any) => (
+              <a href={`/klaster/${href}`} key={title} className="block">
+                <SubunitKlasterCard
+                  image={image}
+                  title={title}
+                  description={description}
+                />
+              </a>
+            ))}
           </div>
         </Container>
       </div>
