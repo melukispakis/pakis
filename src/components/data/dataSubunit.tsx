b44473388af2c328a8ba365477edf5c229c68849
prasetyoedi <prasetyoedi451@gmail.com>
2025-07-24 13:58:22 +0700

    feat: update dataSubunit to correct titles, hrefs, and descriptions for Gerotan, Semimpen, and Kragilan

--- src/components/data/dataSubunit.tsx
+++ src/components/data/dataSubunit.tsx
@@ -1,23 +1,23 @@
 export const dataSubunit = [
   {
-    image: "/gerdu/card.jpg",
-    title: "Kragilan",
-    href: "gerdu",
-    description:
-      "Sub unit Pogalan 2 (Dusun Gerdu) pada KKN PPM UGM berfokus pada pengembangan masyarakat lokal melalui program pendidikan, kesehatan, dan pemberdayaan ekonomi dan wisata.",
-  },
-  {
     image: "/pucung/card.jpg",
     title: "Gerotan",
-    href: "pucung",
+    href: "gerotan",
     description:
-      "Sub unit Pogalan 1 (Dusun Pucung) pada KKN PPM UGM fokus pada pengembangan masyarakat melalui berbagai program pemberdayaan, pendidikan, dan infrastruktur lokal.",
+      "Sub unit Ketundan 1 (Dusun Gerotan) pada KKN PPM UGM fokus pada pengembangan masyarakat melalui berbagai program pemberdayaan, pendidikan, dan infrastruktur lokal.",
   },
   {
-    image: "/semampiran/card.jpg",
+    image: "/semimpen/card.jpg",
     title: "Semimpen",
-    href: "semampiran",
+    href: "semimpen",
     description:
-      "Sub unit Ketundan 1 pada KKN PPM UGM fokus pada pengembangan masyarakat lokal melalui program pemberdayaan ekonomi, pendidikan, dan kesehatan berbasis kebutuhan dusun.",
+      "Sub unit Ketundan 2 (Dusun Semimpen) pada KKN PPM UGM fokus pada pengembangan masyarakat lokal melalui program pemberdayaan ekonomi, pendidikan, dan kesehatan berbasis kebutuhan dusun.",
+  },
+  {
+    image: "/gerdu/card.jpg",
+    title: "Kragilan",
+    href: "kragilan",
+    description:
+      "Sub unit Kragilan (Dusun Kragilan) pada KKN PPM UGM berfokus pada pengembangan masyarakat lokal melalui program pendidikan, kesehatan, dan pemberdayaan ekonomi dan wisata.",
   },
 ];
