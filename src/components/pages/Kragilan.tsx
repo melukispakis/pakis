4ef937f75d26eceb5101d17670718820633e3533
prasetyoedi <prasetyoedi451@gmail.com>
2025-07-27 18:47:03 +0700

    up perbaikan tampilan Kragilan

--- src/components/pages/Kragilan.tsx
+++ src/components/pages/Kragilan.tsx
@@ -54,8 +54,8 @@ export function Kragilan() {
       />
       <div data-aos="fade-up">
         <Container className="mt-[101px] font-jakarta">
-          <H1 className="font-bold">Let’s See Our Sub Unit Kragilan!</H1>
-          <H3 className="pt-[24px]">
+          <H1>Let’s See Our Sub Unit Kragilan!</H1>
+          <H3>
             Berikut adalah program kerja yang telah dilaksanakan oleh sub unit
             Kragilan
           </H3>
